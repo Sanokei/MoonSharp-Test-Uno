f9ae8cf92f53f39c43436feec162212c5ddf41e2
Sano <36809574+Sanokei@users.noreply.github.com>
2022-02-28 17:33:07 -0500

    Fully working Checkpoint

--- Assets/Scripts/Computer/Desktop/TextIconInventory.cs
+++ Assets/Scripts/Computer/Desktop/TextIconInventory.cs
@@ -2,7 +2,4 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Icon/Inventory", fileName = "Inventory.asset")]
 [System.Serializable]
-public class TextIconInventory : Inventory<TextIcon>
-{
-
-}
+public class TextIconInventory : Inventory<TextIcon>{}
